c4aa8e879f8d2579b816eec54f54d5807b1fcb76
killzdesu <killzdesu@gmail.com>
2022-03-24 08:46:56 +0700

    add Github Pages in /gh-pages

---
+++ gh-pages/assets/_...all_.ad51d7a5.js
@@ -0,0 +1,1 @@
+import{e,C as n,o as s,g as a,t as r,j as c}from"./vendor.bcae0d2e.js";import{b as o}from"./app.444fbc4f.js";const f=e({setup(p){const{t}=n();return(i,u)=>(s(),a("div",null,r(c(t)("not-found")),1))}});typeof o=="function"&&o(f);export{f as default};
